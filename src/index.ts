bb466906ff77196328be53f21391da1b27356426
alexvictoor <alexvictoor@gmail.com>
2016-02-10 22:36:59 +0100

    prune the code...

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import Complex from './Complex'
 import * as rx from 'rx.all'
-import { keysToCommands, plugRover } from './pipeline';
-import { Rover, Command } from './rover';
 
-let keys : rx.Observable<number>  = rx.Observable
-  .fromEvent(document, "keydown")
-  .pluck("keyCode")
-  .map(k => k as number);
+document.getElementById("container").innerHTML = 0 + " " + 0;
 
-plugRover(keysToCommands(keys)).subscribe(rover => {
-  document.getElementById("container").innerHTML = rover.position.real + " " + rover.position.im;
-});
-
-plugRover(keysToCommands(keys)).subscribe(rover => {
-  let roverDiv = document.getElementById("rover");
-  roverDiv.innerHTML = rover.way.real + "," + rover.way.im;
-  roverDiv.style.left = (window.innerWidth/2 + rover.position.real) + "px";
-  roverDiv.style.top = (window.innerHeight/2 - rover.position.im) + "px";
-});
+let roverDiv = document.getElementById("rover");
+roverDiv.innerHTML = "Mars";
+roverDiv.style.left = (window.innerWidth/2 + 0) + "px";
+roverDiv.style.top = (window.innerHeight/2 - 0) + "px";
